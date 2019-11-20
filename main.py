e36f86c864fad5ffa00a6d6e44200960c9281937
Cameron Brown <browncameron@browncameron-macbookpro.roam.corp.google.com>
2019-11-20 14:33:44 +0000

    added dummy data for site

--- main.py
+++ main.py
@@ -15,9 +15,26 @@ db = TCDatabaseSQLite(DB_PATH)
 app = Flask(__name__)
 
 
+results = [
+    {
+        "buddy_id": 5454,
+        "buddy_name": "Cameron Brown",
+        "buddy_description": "Hey! I work on the ML Kit team.",
+        "photo_url": "https://picsum.photos/330/200",
+        "programme_id": 5454,
+        "programme_title": "Software Engineering Apprenticeship",
+        "programme_description": "Work on a team of Googlers as you study in your 20% time",
+        "role": "Apprentice SWE",
+        "comany_id": 5435,
+        "company_name": "Google",
+        "location": "London, UK",
+    },
+]
+
+
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', results=results)
 
 
 if __name__ == '__main__':

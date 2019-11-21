8457549552ff5a39518884661c9ffc347ff17b92
Cameron Brown <browncameron@browncameron-macbookpro.roam.corp.google.com>
2019-11-21 12:42:42 +0000

    added filtering implentation

--- main.py
+++ main.py
@@ -18,11 +18,27 @@ app = Flask(__name__)
 @app.route('/')
 def results():
     session = db.new_session()
-    results = session.query(Buddy)
-    query = request.args.get('q')
-    if query:
-        results.filter(Buddy.description.like("%" + query + "%"))
-    return render_template('index.html', results=results)
+
+    q = session.query(Company).all()
+    companies = []
+    for company in q:
+        companies.append(company.company_name)
+
+    company_query = request.args.get('company')
+    if not company_query:
+        results = (session.query(Buddy, ApprenticeshipProgramme, Company)
+              .filter(Buddy.apprenticeship == ApprenticeshipProgramme.id)
+              .filter(ApprenticeshipProgramme.company == Company.id)
+              .all())
+    else:
+        results = (session.query(Buddy, ApprenticeshipProgramme, Company)
+              .filter(Buddy.apprenticeship == ApprenticeshipProgramme.id)
+              .filter(ApprenticeshipProgramme.company == Company.id)
+              .filter(Company.company_name.like("%" + company_query + "%"))
+              .all())
+
+    return render_template('index.html', results=results, company=company_query, 
+        companies=companies)
 
 
 if __name__ == '__main__':

1ba89393ad27372ca575db0e9f9b548fc2bbdbc1
Cameron Brown <browncameron@browncameron-macbookpro.roam.corp.google.com>
2019-11-21 10:43:59 +0000

    added dummydata.sql

---
+++ dummydata.sql
@@ -0,0 +1,17 @@
+-- SQLite
+INSERT INTO apprenticeshipprogramme
+VALUES
+(1, 'Level 6 Data Science', 'Learn how statistics and asking the right...',	'London', 'Data Scientist', 1),
+(2, 'Level 4 Data Analyst', 'Learn how data can be...', 'London', 'Data Analyst', 2),
+(3, 'Bsc Digital Solutions', 'Be part of a team that collaborates and finds...', 'Reading', 'Tech Consultant', 3);
+
+INSERT INTO company
+VALUES
+(1, 'Google'),
+(2, 'Salesforce'),
+(3, 'Deloitte');
+
+INSERT INTO `buddy` VALUES 
+(1 , 'Morrie' , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus tincidunt nunc vitae cursus.' , 'https://images.app.goo.gl/5ZCo8iUoyTyymm8D6' , 3  ),
+(2 , 'Clyde' , 'Praesent ornare ultrices malesuada. Vestibulum ornare dolor vitae ex sagittis tincidunt.' , 'https://images.app.goo.gl/CPu92aF5mpR9K3Yw8' , 1), 
+(3 , 'Darbie' , '' , 'https://images.app.goo.gl/8bAhMDvvBcm8PKqDA' , 2);

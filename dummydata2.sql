b4a500ed27fdcc55274889438b08540d06a6404e
Cameron Brown <browncameron@browncameron-macbookpro.roam.corp.google.com>
2019-11-21 12:01:59 +0000

    added new dummy data

---
+++ dummydata2.sql
@@ -0,0 +1,35 @@
+-- SQLite
+INSERT INTO apprenticeshipprogramme
+VALUES
+(1, 'Level 6 Data Science', 'Learn how statistics and asking the right...', 'London', 'Data Scientist', 1),
+(2, 'Level 4 Data Analyst', 'Learn how data can be...', 'London', 'Data Analyst', 2),
+(3, 'Bsc Digital Solutions', 'Be part of a team that collaborates and finds...', 'Reading', 'Tech Consultant', 3),
+(4,  'Level 6 Data Engineer', 'Learn how data and asking the right...', 'London', 'Data Engineer', 4),
+(5,  'Level 4 Software Engineer', 'Learn how programming and asking the right...',	'London', 'Software Engineer', 5),
+(6,  'Level 6 Data Science', 'Learn how statistics and asking the right...', 'London', 'Data Scientist', 6),
+(7,  'Level 4 Product Developer', 'Learn how design and asking the right...', 'London', 'Product Developer', 7),
+(8,  'Level 4 Data Management', 'Learn how statistics and asking the right...',	'London', 'Data Librarian', 8);
+
+INSERT INTO company
+VALUES
+(1, 'Google'),
+(2, 'Salesforce'),
+(3, 'Deloitte'),
+(4, 'IG'),
+(5, 'Facebook'),
+(6, 'Amazon'),
+(7, 'Barclays'),
+(8, 'EY');
+
+
+INSERT INTO `buddy` VALUES 
+(1 , 'Morrie' , 'Digital Degree apprentice, Math, English, Computer Science' , 'https://image.shutterstock.com/image-photo/isolated-shot-young-handsome-male-600w-762790210.jpg' , 3  ),
+(2 , 'Clyde' , 'Software developer apprentice, Math, English, Computer Science' , 'https://image.shutterstock.com/image-photo/smiling-bearded-young-male-model-600w-788313199.jpg' , 1), 
+(3 , 'Darbie' , 'Hardware engineer apprentice, Math, English, Engineering' , 'https://media.istockphoto.com/photos/portrait-smile-of-woman-on-isolated-white-background-picture-id519919540' , 2),
+(4 , 'Latrina' , 'Web development apprentice, Math, English, Computer Science' , 'https://media.istockphoto.com/photos/close-up-of-positive-young-african-woman-in-glasses-picture-id1056240182' , 4),
+(5 , 'Kippie' , 'Java developer apprentice, Math, English, Computer Science' , 'https://image.shutterstock.com/image-photo/horizontal-portrait-happy-male-enterpreneur-600w-735022123.jpg' , 3),
+(6 , 'Tynan' , 'Junior Developer, Math, Computer Science' , 'https://image.shutterstock.com/image-photo/man-beard-gray-tshirt-600w-1452309212.jpg' , 5),
+(7 , 'Bertie' , 'Degree apprentice, English, Computer Science' , 'https://image.shutterstock.com/image-photo/confidence-business-concept-portrait-charming-600w-1043571091.jpg' , 2),
+(8 , 'Cissiee' , 'Senior Developer, Math, English, Computer Science' , 'https://image.shutterstock.com/image-photo/portrait-young-tender-redhead-teenage-600w-561625168.jpg' , 7),
+(9 , 'Willem' , 'Degree apprentice, Math, English, Computer Science' , 'https://image.shutterstock.com/image-photo/portrait-handsome-young-man-casual-600w-1049220308.jpg' , 8),
+(10 , 'Verene' , 'Technical Lead, Computer Science' , 'https://image.shutterstock.com/image-photo/picture-stylish-trendylooking-young-woman-600w-708329644.jpg' , 6);

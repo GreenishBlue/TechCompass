8e915360bea44204cfbf857742a549a0a2216649
Cameron Brown <browncameron@browncameron-macbookpro.roam.corp.google.com>
2019-11-21 10:44:23 +0000

    added basic sql query to results route

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from db import TCDatabaseSQLite
 from models import *
@@ -15,25 +15,13 @@ db = TCDatabaseSQLite(DB_PATH)
 app = Flask(__name__)
 
 
-results = [
-    {
-        "buddy_id": 5454,
-        "buddy_name": "Cameron Brown",
-        "buddy_description": "Hey! I work on the ML Kit team.",
-        "photo_url": "https://picsum.photos/330/200",
-        "programme_id": 5454,
-        "programme_title": "Software Engineering Apprenticeship",
-        "programme_description": "Work on a team of Googlers as you study in your 20% time",
-        "role": "Apprentice SWE",
-        "comany_id": 5435,
-        "company_name": "Google",
-        "location": "London, UK",
-    },
-]
-
-
 @app.route('/')
-def home_page():
+def results():
+    session = db.new_session()
+    results = session.query(Buddy)
+    query = request.args.get('q')
+    if query:
+        results.filter(Buddy.description.like("%" + query + "%"))
     return render_template('index.html', results=results)
 
 
